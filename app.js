dcc5b07aabb398f9a73313097bbbee8953165aa9
gasprod <gasprod13@gmail.com>
2017-06-09 15:37:59 +0200

    package and grunt

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require("express"),
 	app = express();
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.resolve(__dirname, "public")));
+app.use(express.static(path.resolve(__dirname, "tmp/build")));
+
 app.set("port", process.env.PORT || 3000);
 
 app.get("/", function(req, res) {
-  res.send("Hello from Heroku & Exress & NodeJS world ! Hello Martinka ! Schmatz ! @GasparskY");
+  res.render('index');
+    console.log('index rendered ... '.rainbow);
 });
 
 app.listen(app.get("port"), function() {
